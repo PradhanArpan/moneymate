fb11a52715950c1eac47904c23e6d8fd4a677bce
PradhanArpan <dr.arpanpradhan@gmail.com>
2026-06-27 23:12:28 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const V="mm-v6-2";
+const V="mm-v6-4";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(["/","/index.html"])));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
